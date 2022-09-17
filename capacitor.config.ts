cbf6646e1e6afc45f00d40209e2365c0fc94c8cd
Kuzafri <kuzafri@gmail.com>
2022-09-17 18:31:31 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mall-facility',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
